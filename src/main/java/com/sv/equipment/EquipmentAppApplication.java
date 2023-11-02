a8c5ecfcd83748779fa61dd8d84749547100b277
ivanreyess <melvin_reyes@unicomer.com>
2023-11-02 13:29:07 -0600

    :tada: first commit

---
+++ src/main/java/com/sv/equipment/EquipmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.sv.equipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipmentAppApplication.class, args);
+	}
+
+}
